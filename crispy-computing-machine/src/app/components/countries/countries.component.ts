9c5a4727906f8a9ed0d676e731c3966b3b11b6e6
Keith McCartney <keithtmccartney@hotmail.com>
2022-12-03 08:17:22 +0000

    Committing angular material changes

--- crispy-computing-machine/src/app/components/countries/countries.component.ts
+++ crispy-computing-machine/src/app/components/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-countries',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountriesComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'name',
+    'capital',
+    'currency',
+    'emoji',
+    'phone',
+    'continent',
+  ];
+
+  dataSource$ = this.countriesService.getCountries();
+
+  constructor(private countriesService: CountriesService) { }
 
   ngOnInit(): void {
   }
